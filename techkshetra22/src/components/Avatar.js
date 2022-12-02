0ecd491f718885c449544ba5d5685370e3367213
peterjohnson1000 <peterj1298@gmail.com>
2022-12-02 19:22:52 +0530

    Made Footer and Dashboard responsive partially

--- techkshetra22/src/components/Avatar.js
+++ techkshetra22/src/components/Avatar.js
@@ -1,52 +1,52 @@
-import { useState, useEffect } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import { useState, useEffect } from "react";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 import { Link } from "react-router-dom";
 import { Button } from "@cred/neopop-web/lib/components";
 
 const Avatar = () => {
-    const [photoURL, setPhotoURL] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            setPhotoURL(firebase.auth().currentUser.photoURL);
-        }
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setPhotoURL(firebase.auth().currentUser.photoURL);
+    }
+  });
 
-    useEffect(() => {
-        const resetDP = firebase.auth().onAuthStateChanged(user => {
-          setPhotoURL("");
-        });
-        return () => resetDP();
-    }, []);
+  useEffect(() => {
+    const resetDP = firebase.auth().onAuthStateChanged((user) => {
+      setPhotoURL("");
+    });
+    return () => resetDP();
+  }, []);
 
-    if(photoURL !== "")
-        return (
-            <div>
-                <Link to="/dashboard">
-                    <img
-                        className="h-12 rounded-full mx-8 md:mx-1 md:mr-4 smm:h-8 smm:ml-4 sm:mr-2"
-                        src={photoURL}
-                        alt="profile"
-                    />
-                </Link>
-            </div>
-        );
-    else
-        return (
-            <div>
-                <Link to="/login">
-                    <Button
-                        variant="primary"
-                        kind="elevated"
-                        size="big"
-                        colorMode="light"
-                        >
-                        Login
-                    </Button>
-                </Link>
-            </div>
-        )
-}
+  if (photoURL !== "")
+    return (
+      <div>
+        <Link to="/dashboard">
+          <img
+            className="h-12 rounded-full mx-8 md:mx-1 md:mr-4 smm:h-8 smm:ml-4 sm:mr-2"
+            src={photoURL}
+            alt="profile"
+          />
+        </Link>
+      </div>
+    );
+  else
+    return (
+      <div>
+        <Link to="/login">
+          <Button
+            variant="primary"
+            kind="elevated"
+            size="big"
+            colorMode="light"
+          >
+            Login
+          </Button>
+        </Link>
+      </div>
+    );
+};
 
 export default Avatar;
--- techkshetra22/src/components/Dashboard.js
+++ techkshetra22/src/components/Dashboard.js
@@ -1,77 +1,78 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Slider from "./Slider";
 import RegisteredEvents from "./RegisteredEvents";
 import { Button } from "@cred/neopop-web/lib/components";
 import { Link } from "react-router-dom";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 const Dashboard = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [photoURL, setPhotoURL] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            setName(firebase.auth().currentUser.displayName);
-            setEmail(firebase.auth().currentUser.email);
-            setPhotoURL(firebase.auth().currentUser.photoURL);
-        }
-    })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      setName(firebase.auth().currentUser.displayName);
+      setEmail(firebase.auth().currentUser.email);
+      setPhotoURL(firebase.auth().currentUser.photoURL);
+    }
+  });
 
-    const words = [
-        "TECHKSHETRA",
-        "*",
-        "2022",
-        "*",
-        "WORKSHOPS",
-        "*",
-        "TECHTALKS",
-        "*",
-        "FOOD",
-        "*",
-        "BANDSLAM",
-        "*",
-    ];
+  const words = [
+    "TECHKSHETRA",
+    "*",
+    "2022",
+    "*",
+    "WORKSHOPS",
+    "*",
+    "TECHTALKS",
+    "*",
+    "FOOD",
+    "*",
+    "BANDSLAM",
+    "*",
+  ];
 
-    return (
-        <div className="w-full mt-24">
-        <Slider words={words} />
-        <div className="flex flex-col w-fit mx-auto my-10">
-
-            <div className="flex justify-between items-center">
-                <div className="flex items-center">
-                    <img
-                        className="h-12 rounded-full mr-8"
-                        src={photoURL}
-                        alt="profile"
-                    />
-                    <div>
-                        <h4 className="text-3xl">{name}</h4>
-                        <p>{email}</p>
-                    </div>
-                </div>
-                <Link to="/"><Button
-                    variant="primary"
-                    kind="elevated"
-                    size="big"
-                    colorMode="light"
-                    onClick={() => firebase.auth().signOut()}
-                    >
-                    Logout
-                </Button></Link>
-            </div>
-
+  return (
+    <div className="w-full mt-24">
+      <Slider words={words} />
+      <div className="flex flex-col w-1/2 mx-auto my-10 lg:w-2/3">
+        <div className="flex justify-between items-center lg:flex-col">
+          <div className="flex items-center my-10">
+            <img
+              className="h-12 rounded-full mr-8"
+              src={photoURL}
+              alt="profile"
+            />
             <div>
-                <h4 className="text-4xl font-bold my-10">Registered Events</h4>
-                <div>
-                    <RegisteredEvents />
-                </div>
+              <h4 className="text-3xl">{name}</h4>
+              <p>{email}</p>
             </div>
+          </div>
+          <Link to="/">
+            <Button
+              variant="primary"
+              kind="elevated"
+              size="big"
+              colorMode="light"
+              onClick={() => firebase.auth().signOut()}
+            >
+              Logout
+            </Button>
+          </Link>
         </div>
-        <Slider words={words} />
+
+        <div>
+          <h4 className="text-4xl font-bold my-10">Registered Events</h4>
+          <div>
+            <RegisteredEvents />
+          </div>
         </div>
-    );
+      </div>
+      <Slider words={words} />
+    </div>
+  );
 };
 
 export default Dashboard;
